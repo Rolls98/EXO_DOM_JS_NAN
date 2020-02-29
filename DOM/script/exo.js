2ca63bdfe1b4c9ca1b8d5caf972f32afd96bfd41
rolls98 <toureb16@gmail.com>
2020-02-29 17:31:53 +0100

    Fin du jeu de devinette

--- DOM/script/exo.js
+++ DOM/script/exo.js
@@ -4,12 +4,28 @@ function aleatoire(min, max) {
     : "impossible que le min soit superieur au max";
 }
 
-function changeImage(img) {
-  img.setAttribute("src", "images/" + aleatoire(1, 5) + ".jpeg");
+function Game(img, dev) {
+  var nbAl = aleatoire(1, 5);
+  const msg = document.getElementById("msg");
+  if (dev.includes(nbAl)) {
+    if (dev.includes(".jpeg")) {
+      msg.innerHTML = "Bravo vous avez reussi à deviner l'image";
+    } else {
+      msg.innerHTML = "Veuillez saisir un bon nom example(1.jpeg)";
+    }
+  } else {
+    msg.innerHTML =
+      "Desolé vous avez echoué :) <br> la bonne image est : " + nbAl + ".jpeg";
+  }
+
+  img.setAttribute("src", "images/" + nbAl + ".jpeg");
 }
 
 var image = document.getElementsByTagName("img")[0];
 
-addEventListener("click", function() {
-  changeImage(image);
+var js = document.getElementById("jouer");
+
+js.addEventListener("click", function() {
+  var input = document.getElementsByTagName("input")[0].value;
+  Game(image, input);
 });
