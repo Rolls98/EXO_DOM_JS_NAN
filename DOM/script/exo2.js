20e80c72112c955897182b1c54591b1709b90bf1
rolls98 <toureb16@gmail.com>
2020-02-29 18:04:42 +0100

    separation de l'exo 2

---
+++ DOM/script/exo2.js
@@ -0,0 +1,15 @@
+function aleatoire(min, max) {
+    return min < max
+      ? parseInt(Math.random() * (max + 1 - min)) + min
+      : "impossible que le min soit superieur au max";
+  }
+  
+  function changeImage(img) {
+    img.setAttribute("src", "images/" + aleatoire(1, 5) + ".jpeg");
+  }
+  
+  var image = document.getElementsByTagName("img")[0];
+  
+  addEventListener("click", function() {
+    changeImage(image);
+  });
